c70f065e90c25e1aef9d8ec7e57ddc2bbddd1e6c
Lizzie <lizzie.briggs10@gmail.com>
2020-06-09 17:38:14 +0100

    Navmesh agent can move to set destination
    Created test scene with movable NPC

---
+++ Whodunit/Assets/Scripts/Navmesh/MoveNavMeshAgent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
+
+namespace NavMesh
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MoveNavMeshAgent : MonoBehaviour
+    {
+        [Header("NavMesh")]
+        public Vector3 target;
+        public float agentSpeed;
+        private NavMeshAgent _navMeshAgent;
+        
+        private void Start()
+        {
+            if(GetComponent<NavMeshAgent>()) _navMeshAgent = GetComponent<NavMeshAgent>();
+            _navMeshAgent.speed = agentSpeed;
+        }
+
+        private void Update()
+        {
+            if(transform.position != target)
+                MoveAgentTo(target);
+        }
+
+        private void MoveAgentTo(Vector3 location)
+        {
+            _navMeshAgent.SetDestination(location);
+        }
+    }
+}

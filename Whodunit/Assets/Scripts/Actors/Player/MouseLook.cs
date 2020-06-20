219e3cdf0aee11da00b19ed4a49230422592d71a
Lizzie <lizzie.briggs10@gmail.com>
2020-06-20 18:27:20 +0100

    Implemented simple first person player controller
    Isn't restricted by navmesh yet

---
+++ Whodunit/Assets/Scripts/Actors/Player/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Actors.Player
+{
+    public class MouseLook : MonoBehaviour
+    {
+        [SerializeField] private float mouseSensitivity = 100f;
+        [SerializeField] private Transform player;
+
+        private float _xRotation;
+
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            _xRotation -= mouseY;
+            _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+            player.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
---
+++ Whodunit/Assets/Scripts/Actors/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Actors.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 2f;
+        
+        private void FixedUpdate()
+        {
+            MovePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            
+            Vector3 move = new Vector3(x, 0f, z);
+
+            transform.Translate(move * (speed * Time.deltaTime));
+        }
+    }
+}
